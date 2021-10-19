6b376edb7a02ae8f34207a86ecc25f99f0491111
50019348 <91960027+50019348@users.noreply.github.com>
2021-10-19 14:07:28 +0100

    Add files via upload

---
+++ UserInput/UserInput/frmMainMenu.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace UserInput
+{
+    partial class frmMainMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMainMenu = new System.Windows.Forms.Label();
+            this.lblForeName = new System.Windows.Forms.Label();
+            this.txtForename = new System.Windows.Forms.TextBox();
+            this.lblSurname = new System.Windows.Forms.Label();
+            this.txtSurename = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.lblAge = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblMainMenu
+            // 
+            this.lblMainMenu.AutoSize = true;
+            this.lblMainMenu.Location = new System.Drawing.Point(320, 89);
+            this.lblMainMenu.Name = "lblMainMenu";
+            this.lblMainMenu.Size = new System.Drawing.Size(151, 16);
+            this.lblMainMenu.TabIndex = 0;
+            this.lblMainMenu.Text = "Windows Form - User Input";
+            this.lblMainMenu.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblForeName
+            // 
+            this.lblForeName.AutoSize = true;
+            this.lblForeName.Location = new System.Drawing.Point(357, 137);
+            this.lblForeName.Name = "lblForeName";
+            this.lblForeName.Size = new System.Drawing.Size(92, 16);
+            this.lblForeName.TabIndex = 1;
+            this.lblForeName.Text = "Enter Forename:";
+            // 
+            // txtForename
+            // 
+            this.txtForename.Location = new System.Drawing.Point(446, 134);
+            this.txtForename.Name = "txtForename";
+            this.txtForename.Size = new System.Drawing.Size(166, 21);
+            this.txtForename.TabIndex = 2;
+            // 
+            // lblSurname
+            // 
+            this.lblSurname.AutoSize = true;
+            this.lblSurname.Location = new System.Drawing.Point(357, 176);
+            this.lblSurname.Name = "lblSurname";
+            this.lblSurname.Size = new System.Drawing.Size(86, 16);
+            this.lblSurname.TabIndex = 3;
+            this.lblSurname.Text = "Enter Surname:";
+            // 
+            // txtSurename
+            // 
+            this.txtSurename.Location = new System.Drawing.Point(446, 173);
+            this.txtSurename.Name = "txtSurename";
+            this.txtSurename.Size = new System.Drawing.Size(166, 21);
+            this.txtSurename.TabIndex = 4;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(334, 226);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(118, 35);
+            this.btnSave.TabIndex = 5;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(494, 226);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(118, 35);
+            this.btnClear.TabIndex = 6;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(446, 200);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(166, 21);
+            this.txtAge.TabIndex = 8;
+            this.txtAge.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // lblAge
+            // 
+            this.lblAge.AutoSize = true;
+            this.lblAge.Location = new System.Drawing.Point(358, 204);
+            this.lblAge.Name = "lblAge";
+            this.lblAge.Size = new System.Drawing.Size(61, 16);
+            this.lblAge.TabIndex = 7;
+            this.lblAge.Text = "Enter Age:";
+            this.lblAge.Click += new System.EventHandler(this.lblAge_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(334, 267);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(278, 35);
+            this.btnExit.TabIndex = 9;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // frmMainMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(933, 554);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.lblAge);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtSurename);
+            this.Controls.Add(this.lblSurname);
+            this.Controls.Add(this.txtForename);
+            this.Controls.Add(this.lblForeName);
+            this.Controls.Add(this.lblMainMenu);
+            this.Font = new System.Drawing.Font("Microsoft YaHei UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "frmMainMenu";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmMainMenu_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMainMenu;
+        private System.Windows.Forms.Label lblForeName;
+        private System.Windows.Forms.TextBox txtForename;
+        private System.Windows.Forms.Label lblSurname;
+        private System.Windows.Forms.TextBox txtSurename;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.Label lblAge;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ UserInput/UserInput/frmMainMenu.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UserInput
+{
+    public partial class frmMainMenu : Form
+    {
+        public frmMainMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMainMenu_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            DialogResult Result;
+
+            Result = MessageBox.Show("Are you sure you wish to clear?", "Clear", MessageBoxButtons.YesNo);
+        
+            if (Result == DialogResult.Yes)
+            {
+                txtForename.Text = null;
+                txtSurename.Text = null;
+                txtAge.Text = null;
+            }
+        
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string strForename = txtForename.Text;
+            string strSurname = txtSurename.Text;
+            int intAge = Convert.ToInt32(txtAge.Text);
+
+            MessageBox.Show("First Name: " + strForename + " Second Name: " + strSurname + "\nAge", "User Input");
+
+
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult Result;
+
+            Result = MessageBox.Show("Are you sure you wish to Exit?", "Exit", MessageBoxButtons.YesNo);
+
+            if (Result == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+
+                
+        }
+
+        private void lblAge_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
